865d6e169ca1d65a96de89222305395eb1b86480
wal-wizard <goneisgone@hotmail.com>
2023-10-03 18:50:37 -0300

    ETL: Extract Data with Node.js

--- Index.js
+++ Index.js
@@ -1,15 +1,31 @@
-const rp = require('request-promise');
+const rp = require("request-promise");
 
-const EXO_API_URL = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI';
+const EXO_API_URL =
+    "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI";
 
+const getAllPlanets = () =>
+    rp({
+        uri: EXO_API_URL,
+        method: "GET",
+        qs: {
+            table: "exoplanets",
+            format: "json"
+        },
+        json: true
+    });
 
+const startEtlPipelines = async () => {
+    try {
+        //Extract Planets
+        const planets = await getAllPlanets();
+        console.log(`Extracted ${planets.length} planets from de API`);
 
-const getAllPlanet = () => rp({
-    uri: EXO_API_URL,
-    method: 'GET',
-    qs: {
-        table: 'exoplanets',
-        format: 'json',
-    },
-    json: true,
-})  
+        //TODO Tranform Step
+
+        //TODO Load Step
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startEtlPipelines();
